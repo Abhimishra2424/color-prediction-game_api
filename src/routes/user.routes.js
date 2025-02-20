6385cee9c681cad6a15ef5c75133dbabad9e7a2b
AbhishekMishra <abmishra700@gmail.com>
2025-02-20 15:47:24 +0530

    Refactor user routes and controller to enhance user retrieval with authentication middleware; update user creation to ensure consistent password hashing.

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const UserController = require("../controllers/user.controller");
+const { authMiddleware } = require("../middleware/authMiddleware");
 
 router.post("/register", UserController.register);
 router.post("/login", UserController.login);
 router.post("/create", UserController.createUser);
 router.get("/", UserController.getAllUsers);
-router.get("/:id", UserController.getUserById);
+router.get("/get", authMiddleware, UserController.getUserById);
 router.put("/:id", UserController.updateUser);
 router.delete("/:id", UserController.deleteUser);
 
